946dcc5d7cd224fd58f5f8f3f4703f43ee86264b
nemerdiaz <54965009+nemerdiaz@users.noreply.github.com>
2021-01-18 17:15:33 -0300

    revisar

--- views/index.views.php
+++ views/index.views.php
@@ -3,7 +3,7 @@
 	<div class="container"> 		
 			<!-- <div class="row">
 				<div class="col"> -->
-					<h2>SUBIR EXCEL</h2>			
+					<h2>SUBIR EXCEL</h2>	
 
 					<?php 
 					if (isset($_POST['enviar']))
@@ -26,20 +26,16 @@
 						)";
 					
 						$conexion->query($q);
-					   }
-					
+					   }					
 						  fclose($handle);
 					   }
 					}
 					?>
-
-
-
 					<form enctype="multipart/form-data" method="post" action="">
 						CSV File: <input type="file" name="file" id="file">
 						<input type="submit" value="Enviar" name="enviar">
 					</form>
-
+					<!-- https://www.baulphp.com/importar-archivo-de-excel-a-mysql-usando-php/ -->
 	</div>
 	<?php require 'footer.php'; ?>
 
