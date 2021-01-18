2cb6b1c643d500017d7753d5b17ef3f1ea01723e
nemerdiaz <54965009+nemerdiaz@users.noreply.github.com>
2021-01-18 15:01:20 -0300

    IP actualizada

--- admin/config.php
+++ admin/config.php
@@ -3,7 +3,7 @@
 define('RUTA', 'http://localhost:8080/Excel/');
 
 $bd_config = array (
-	'server' => 'localhost',
+	'server' => '192.168.1.137',
 	'usuario' => 'root',
 	'pass' => '',
 	'basedatos' => 'blacklist'
